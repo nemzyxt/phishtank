429fa42f06229333b9a906010c9ea9b2cae3d5df
Nemuel Wainaina <nemuelwainaina@proton.me>
2024-01-17 07:37:18 +0300

    Implement the API client

--- api/api.go
+++ api/api.go
@@ -1,1 +1,21 @@
 package api
+
+const (
+	API_URL = "http://checkpoint.phishtank.com/checkurl"
+)
+
+type client struct {
+	ApiKey string
+	UserAgent string
+}
+
+func NewClient(apiKey string, userAgent string) client {
+	return client {
+		ApiKey: apiKey,
+		UserAgent: userAgent,
+	}
+}
+
+func CheckURL(url string) {
+	
+}
